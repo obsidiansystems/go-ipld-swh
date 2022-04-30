cf82d4b0d469cd308c05e3cc28801656e5b6b42e
John Ericson <John.Ericson@Obsidian.Systems>
2022-04-30 18:04:23 -0400

    Adapt schema for SWH snapshots

--- gen/gen.go
+++ gen/gen.go
@@ -18,52 +18,53 @@ func main() {
 		},
 	}
 
-	ts.Accumulate(schema.SpawnString("String"))
+	ts.Accumulate(schema.SpawnString("String")) // Needed for Union Tag
+	ts.Accumulate(schema.SpawnString("BranchName"))
 	ts.Accumulate(schema.SpawnLink("Link"))
 
-	ts.Accumulate(schema.SpawnList("String_List", "String", false))
-
-	ts.Accumulate(schema.SpawnStruct("PersonInfo", []schema.StructField{
-		schema.SpawnStructField("date", "String", false, false),
-		schema.SpawnStructField("timezone", "String", false, false),
-		schema.SpawnStructField("email", "String", false, false),
-		schema.SpawnStructField("name", "String", false, false),
+	ts.Accumulate(schema.SpawnStruct("SnapshotBranch_Content", []schema.StructField{
+		schema.SpawnStructField("target", "Link", false, false),
 	}, schema.SpawnStructRepresentationMap(map[string]string{})))
 
-	ts.Accumulate(schema.SpawnString("GpgSig"))
-
-	ts.Accumulate(schema.SpawnStruct("Tag", []schema.StructField{
-		schema.SpawnStructField("object", "Link", false, false),
-		schema.SpawnStructField("type", "String", false, false),
-		schema.SpawnStructField("tag", "String", false, false),
-		schema.SpawnStructField("tagger", "PersonInfo", false, false),
-		schema.SpawnStructField("message", "String", false, false),
+	ts.Accumulate(schema.SpawnStruct("SnapshotBranch_Directory", []schema.StructField{
+		schema.SpawnStructField("target", "Link", false, false),
 	}, schema.SpawnStructRepresentationMap(map[string]string{})))
 
-	ts.Accumulate(schema.SpawnList("Tag_List", "Tag", false))
-
-	ts.Accumulate(schema.SpawnStruct("Commit", []schema.StructField{
-		schema.SpawnStructField("tree", "Tree_Link", false, false),
-		schema.SpawnStructField("parents", "Commit_Link_List", false, false),
-		schema.SpawnStructField("message", "String", false, false),
-		schema.SpawnStructField("author", "PersonInfo", true, false),
-		schema.SpawnStructField("committer", "PersonInfo", true, false),
-		schema.SpawnStructField("encoding", "String", true, false),
-		schema.SpawnStructField("signature", "GpgSig", true, false),
-		schema.SpawnStructField("mergetag", "Tag_List", false, false),
-		schema.SpawnStructField("other", "String_List", false, false),
+	ts.Accumulate(schema.SpawnStruct("SnapshotBranch_Revision", []schema.StructField{
+		schema.SpawnStructField("target", "Link", false, false),
 	}, schema.SpawnStructRepresentationMap(map[string]string{})))
-	ts.Accumulate(schema.SpawnLinkReference("Commit_Link", "Commit"))
-	ts.Accumulate(schema.SpawnList("Commit_Link_List", "Commit_Link", false))
 
-	ts.Accumulate(schema.SpawnBytes("Blob"))
-
-	ts.Accumulate(schema.SpawnMap("Tree", "String", "TreeEntry", false))
-	ts.Accumulate(schema.SpawnStruct("TreeEntry", []schema.StructField{
-		schema.SpawnStructField("mode", "String", false, false),
-		schema.SpawnStructField("hash", "Link", false, false),
+	ts.Accumulate(schema.SpawnStruct("SnapshotBranch_Release", []schema.StructField{
+		schema.SpawnStructField("target", "Link", false, false),
 	}, schema.SpawnStructRepresentationMap(map[string]string{})))
-	ts.Accumulate(schema.SpawnLinkReference("Tree_Link", "Tree"))
+
+	ts.Accumulate(schema.SpawnStruct("SnapshotBranch_Snapshot", []schema.StructField{
+		schema.SpawnStructField("target", "Snapshot_Link", false, false),
+	}, schema.SpawnStructRepresentationMap(map[string]string{})))
+
+	ts.Accumulate(schema.SpawnStruct("SnapshotBranch_Alias", []schema.StructField{
+		schema.SpawnStructField("target", "BranchName", false, false),
+	}, schema.SpawnStructRepresentationMap(map[string]string{})))
+
+	ts.Accumulate(schema.SpawnUnion("SnapshotBranch", []schema.TypeName{
+		"SnapshotBranch_Content",
+		"SnapshotBranch_Directory",
+		"SnapshotBranch_Revision",
+		"SnapshotBranch_Release",
+		"SnapshotBranch_Snapshot",
+		"SnapshotBranch_Alias",
+	}, schema.SpawnUnionRepresentationKeyed(map[string]schema.TypeName{
+		"content": "SnapshotBranch_Content",
+		"directory": "SnapshotBranch_Directory",
+		"revision": "SnapshotBranch_Revision",
+		"release": "SnapshotBranch_Release",
+		"snapshot": "SnapshotBranch_Snapshot",
+		"alias": "SnapshotBranch_Alias",
+	})))
+
+	ts.Accumulate(schema.SpawnMap("Snapshot", "BranchName", "SnapshotBranch", false))
+
+	ts.Accumulate(schema.SpawnLinkReference("Snapshot_Link", "Snapshot"))
 
 	if errs := ts.ValidateGraph(); errs != nil {
 		for _, err := range errs {
@@ -72,5 +73,5 @@ func main() {
 		panic("not happening")
 	}
 
-	gengo.Generate(os.Args[1], "ipldgit", ts, adjCfg)
+	gengo.Generate(os.Args[1], "ipldswh", ts, adjCfg)
 }
